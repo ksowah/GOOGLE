3fbff93e35b2b0a95913d6b76d0f84e909ed1567
ksowah <sowahkelvin640@gmail.com>
2022-02-10 12:08:14 +0000

    update

--- components/Footer.js
+++ components/Footer.js
@@ -4,11 +4,11 @@ const Footer = () => {
   return (
     <footer className="grid w-full divide-y-[1px]
      divide-gray-300 bg-gray-100 text-sm text-gray-500">
-        <div className="px-8 py-3">
+        <div className="px-6 py-3">
             <p>Ghana</p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense p-4 px-6">
             <div className='flex items-center justify-center md:col-span-2 lg:col-span-1 lg:col-start-2'>
                 <GlobeIcon
                     className="h-5 mr-1 text-green-700"
