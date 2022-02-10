4fd1510a070ac84655c0994fbb7b2a2ac31a7dd8
ksowah <sowahkelvin640@gmail.com>
2022-02-10 11:45:21 +0000

    update

--- components/Footer.js
+++ components/Footer.js
@@ -8,12 +8,24 @@ const Footer = () => {
             <p>Ghana</p>
         </div>
 
-        <div>
+        <div className="grid grid-cols-1">
             <div>
                 <GlobeIcon
                     className="h-5 mr-1 text-green-700"
                 />
                 Carbon neutral since 2007
+            </div>
+
+            <div className="flex justify-center">
+                <p>Advertising</p>
+                <p>Business</p>
+                <p>How Search works</p>
+            </div>
+
+            <div>
+                <p>Privacy</p>
+                <p>Terms</p>
+                <p>Settings</p>
             </div>
         </div>
     </footer>
