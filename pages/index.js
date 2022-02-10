f3d7bdeae04fcc506b2ccdee21130430e3629ab9
ksowah <sowahkelvin640@gmail.com>
2022-02-10 10:10:50 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-       <header className='flex justify-between'>
+       <header className='flex justify-between w-full p-5'>
          <div className='flex space-x-4'>
             <p>About</p>
             <p>Store</p>
