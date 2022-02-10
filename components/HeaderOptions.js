a96562796315c618234cab1355917d5a4f958026
ksowah <sowahkelvin640@gmail.com>
2022-02-10 14:41:38 +0000

    update

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -4,15 +4,21 @@ import {
     MapIcon,
     NewspaperIcon,
     PlayIcon,
-    SearchIcon
+    SearchIcon,
+    PhotographIcon
  } from '@heroicons/react/solid'
 
 
 const HeaderOptions = () => {
   return (
-    <div>
+    <div className='flex w-full text-gray-700 justify-evenly items-center'>
         <div className='flex space-x-4'>
             <HeaderOptionsSingular Icon={SearchIcon} title='All' selected/>
+            <HeaderOptionsSingular Icon={PhotographIcon} title='Images' />
+            <HeaderOptionsSingular Icon={PlayIcon} title='Videos' />
+            <HeaderOptionsSingular Icon={NewspaperIcon} title='News' />
+            <HeaderOptionsSingular Icon={MapIcon} title='Maps' />
+            <HeaderOptionsSingular Icon={DotsVerticalIcon} title='More' />
         </div>
         <div className='flex space-x-4 items-center'>
             <p className='link'>Settings</p>
