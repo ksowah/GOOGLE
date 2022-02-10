41209a11b2ad2d5da6f7937b1ad45d7082d9c7d1
ksowah <sowahkelvin640@gmail.com>
2022-02-10 16:12:12 +0000

    update

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,8 +1,19 @@
 
 const SearchResults = ({results}) => {
   return (
-    <div>
-        <p>About {results.searchInformation?.formattedTotalResults} results</p>
+    <div className='mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52'>
+        <p className='text-gray-600 text-md mb-5 mt-3'>
+            About {results.searchInformation?.formattedTotalResults} results ({results.searchInformation?.formattedSearchTime} seconds)
+        </p>
+
+        {results.itams?.map((result) => (
+            <div key={result.link} className='max-w-xl mb-8'>
+                <div>
+
+                </div>
+                <p>{result.snippet}</p>
+            </div>
+        ))}
     </div>
   )
 }
