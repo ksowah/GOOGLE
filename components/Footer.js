66b9d174ee4f4101519ab5ed0fde16394c4c3759
ksowah <sowahkelvin640@gmail.com>
2022-02-10 12:02:01 +0000

    update

--- components/Footer.js
+++ components/Footer.js
@@ -8,21 +8,21 @@ const Footer = () => {
             <p>Ghana</p>
         </div>
 
-        <div className="grid grid-cols-1">
-            <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense">
+            <div className='flex items-center justify-center md:col-span-2 lg:col-span-1 lg:col-start-2'>
                 <GlobeIcon
                     className="h-5 mr-1 text-green-700"
                 />
                 Carbon neutral since 2007
             </div>
 
-            <div className="flex justify-center">
+            <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start ">
                 <p>Advertising</p>
                 <p>Business</p>
                 <p>How Search works</p>
             </div>
 
-            <div>
+            <div className='flex justify-center space-x-8 md:ml-auto'>
                 <p>Privacy</p>
                 <p>Terms</p>
                 <p>Settings</p>
