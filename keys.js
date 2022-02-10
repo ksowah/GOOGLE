85509d9302cce113076b063fded287d48968f7e3
ksowah <sowahkelvin640@gmail.com>
2022-02-10 15:20:50 +0000

    update

--- keys.js
+++ keys.js
@@ -1,3 +1,5 @@
-// API Key
+// API Key https://developers.google.com/custom-search/v1/using_rest
 
-// Context Key
+const Key = 'AIzaSyADuD3OAX9EOBPtd2hagdqo8ETnxsS8oZY'
+
+// Context Key https://cse.google.com/cse/create/new
