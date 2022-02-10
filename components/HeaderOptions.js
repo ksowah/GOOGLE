c79b8a98ef5be16f897d21b8a46e818bd6fe93d4
ksowah <sowahkelvin640@gmail.com>
2022-02-10 14:29:03 +0000

    update

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import HeaderOptionsSingular from "./HeaderOptionsSingular"
+import { 
+    DotsVerticalIcon,
+    MapIcon,
+    NewspaperIcon,
+    PlayIcon,
+    SearchIcon
+ } from '@heroicons/react/solid'
+
 
 const HeaderOptions = () => {
   return (
-    <div>HeaderOptions</div>
+    <div>
+        <div className='flex space-x-4'>
+            <HeaderOptionsSingular Icon={SearchIcon} title='All' selected/>
+        </div>
+        <div className='flex space-x-4 items-center'>
+            <p className='link'>Settings</p>
+            <p className='link'>Tools</p>
+        </div>
+    </div>
   )
 }
 
---
+++ components/HeaderOptionsSingular.js
@@ -0,0 +1,10 @@
+
+const HeaderOptionsSingular = ({}) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default HeaderOptionsSingular
