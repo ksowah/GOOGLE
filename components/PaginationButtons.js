ce0ee04f2e9107ab68b1af7b3de2f6b1ddb24304
ksowah <sowahkelvin640@gmail.com>
2022-02-10 17:00:00 +0000

    update

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -7,7 +7,7 @@ const PaginationButtons = () => {
     const router = useRouter()
     const startIndex = Number(router.query.start)  || 0
   return (
-    <div>
+    <div className='flex max-w-lg justify-between text-blue-700 mb-10'>
         {startIndex >= 10 && (
             <Link href={`/search?term=${router.query.term}&start=${startIndex - 10}`}>
                 <div className='flex flex-grow flex-col items-center cursor-pointer hover:underline'>
