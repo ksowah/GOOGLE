cc33262e39d674164de20daf64ffaa6f66ea17e6
ksowah <sowahkelvin640@gmail.com>
2022-02-10 11:39:55 +0000

    update

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,19 @@ import { GlobeIcon } from "@heroicons/react/solid"
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="grid w-full divide-y-[1px]
+     divide-gray-300 bg-gray-100 text-sm text-gray-500">
         <div className="px-8 py-3">
             <p>Ghana</p>
+        </div>
+
+        <div>
+            <div>
+                <GlobeIcon
+                    className="h-5 mr-1 text-green-700"
+                />
+                Carbon neutral since 2007
+            </div>
         </div>
     </footer>
   )
