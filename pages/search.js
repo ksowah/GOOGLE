26f13cce11b1bfbc295526986c18ecc7cab855fc
ksowah <sowahkelvin640@gmail.com>
2022-02-10 15:49:13 +0000

    update

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import Header from "../components/Header"
 import { Key, context_key } from "../keys"
+import Response from "../Response"
 
 const Search = ({results}) => {
     console.log(results);
@@ -23,9 +24,9 @@ export default Search
 
 
 export const getServerSideProps = async (context)=> {
-    const useDummyData = false
+    const useDummyData = true
 
-    const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${Key}&cx=${context_key}&q=${context.query.term}`
+    const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${Key}&cx=${context_key}&q=${context.query.term}`
     ).then(response => response.json())
 
     return {
