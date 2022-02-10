048d821db866051a1e18bf93c0bd6d8cdc9c9edc
ksowah <sowahkelvin640@gmail.com>
2022-02-10 15:28:12 +0000

    update

--- pages/search.js
+++ pages/search.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import Header from "../components/Header"
+import { Key } from "../keys"
 
 const Search = () => {
   return (
@@ -22,5 +23,6 @@ export default Search
 
 export const getServerSideProps = async (context)=> {
     const useDummyData = false
-    
+
+    const data = await fetch(`https://www.googleapis.com/customsearch/v1?key=${Key}`)
 }
