b7908de52aa3af5dd6ab4a936e168e25f02ca3b1
ksowah <sowahkelvin640@gmail.com>
2022-02-10 13:23:17 +0000

    update

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,8 @@ const Header = () => {
             onClick={()=> router.push('/')}
         />
 
-        <form className='flex border border-gray-200'>
+        <form className='flex border border-gray-200 rounded-full 
+        shadow-lg max-w-3xl px-6 py-3 ml-10 mr-5 items-center'>
             <input type='text' ref={searchInputRef}
                 className={'flex-grow w-full focus:outline-none'}
             />
