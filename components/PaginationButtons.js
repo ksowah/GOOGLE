909aab664d6656719acae3e0ff86efdf65c340de
ksowah <sowahkelvin640@gmail.com>
2022-02-10 16:50:39 +0000

    update

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -1,8 +1,28 @@
+import { useRouter } from "next/router"
+import Link from "next/link"
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid"
 
 const PaginationButtons = () => {
+
+    const router = useRouter()
+    const startIndex = Number(router.query.start)  || 0
   return (
     <div>
-        
+        {startIndex >= 10 && (
+            <Link>
+                <div>
+                    <ChevronLeftIcon/>
+                    <p>Previous</p>
+                </div>
+            </Link>
+        )}
+
+        <Link href={`/search?term=${router.query.term}&start=${}`}>
+        <div>
+            <ChevronRightIcon/>
+            <p>Next</p>
+        </div>
+        </Link>
     </div>
   )
 }
