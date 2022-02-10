18c96ecd62b60e6fbbe0d8ab53a8edb3c8d87c14
ksowah <sowahkelvin640@gmail.com>
2022-02-10 13:37:52 +0000

    update

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import { MicrophoneIcon, SearchIcon, XIcon } from '@heroicons/react/solid'
 import Image from 'next/image'
 import {useRouter} from 'next/router'
 import { useRef } from 'react'
@@ -6,6 +7,13 @@ const Header = () => {
 
     const router = useRouter()
     const searchInputRef = useRef(null)
+
+    const search = (e)=>{
+        e.preventDefault()
+        const term = searchInputRef.current.value
+        if(!term)return
+        router.push(`/search?term=${term}`)
+    }
 
   return (
     <header>
@@ -22,7 +30,23 @@ const Header = () => {
             <input type='text' ref={searchInputRef}
                 className={'flex-grow w-full focus:outline-none'}
             />
+
+        <XIcon
+            className='h-7 text-gray-500 cursor-pointer 
+            sm:mr-3 transition duration-100 transform hover:scale-125'
+            onClick={()=> searchInputRef.current.value=''}
+        />
+        <MicrophoneIcon
+            className={'h-6 mr-3 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300'}
+        />
+        <SearchIcon
+            className='h-6 text-blue-500 hidden sm:inline-flex'
+        />
+
+        <button hidden type='submit' onClick={search} className=''>search</button>
         </form>
+
+       
     </header>
   )
 }
