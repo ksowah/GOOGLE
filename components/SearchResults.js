abb2545eed102adaca38ea0f32867fc6a8085c19
ksowah <sowahkelvin640@gmail.com>
2022-02-10 16:27:30 +0000

    update

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -7,13 +7,13 @@ const SearchResults = ({results}) => {
         </p>
 
         {results.items?.map((result) => (
-            <div key={result.link} className='max-w-xl mb-8'>
-                <div>
+            <div key={result.link} className='max-w-xl mb-8 '>
+                <div className='group'>
                     <a href={result.link} className='text-sm'>
                         {result.formattedUrl}
                     </a>
                     <a href={result.link}>
-                        <h2 className='truncate text-xl text-blue-800 font-medium'>
+                        <h2 className='truncate text-xl text-blue-800 font-medium group-hover:underline'>
                             {result.title}
                         </h2>
                     </a>
