a06527d1d41f7d36ff7a6f93413ea7a720dbe3ee
ksowah <sowahkelvin640@gmail.com>
2022-02-10 11:31:18 +0000

    update

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,9 @@ import { GlobeIcon } from "@heroicons/react/solid"
 const Footer = () => {
   return (
     <footer>
-        
+        <div>
+            <p>Ghana</p>
+        </div>
     </footer>
   )
 }
