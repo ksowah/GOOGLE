259ac721ae778418ad8bb11a865c55aff486e800
ksowah <sowahkelvin640@gmail.com>
2022-02-11 19:01:04 +0000

    update

--- components/Header.js
+++ components/Header.js
@@ -28,31 +28,34 @@ const Header = () => {
             onClick={()=> router.push('/')}
         />
 
-        <form className='flex flex-grow border border-gray-200 rounded-full 
-        shadow-lg max-w-3xl px-6 py-3 ml-10 mr-5 items-center'>
-            <input type='text' ref={searchInputRef}
-                className={'flex-grow w-full focus:outline-none'}
-                defaultValue={router.query.term}
-            />
+            <div className='flex'>
+                    <form className='flex flex-grow border border-gray-200 rounded-full 
+                    shadow-lg max-w-3xl px-6 py-3 ml-10 mr-5 items-center'>
+                    <input type='text' ref={searchInputRef}
+                        className={'flex-grow w-full focus:outline-none'}
+                        defaultValue={router.query.term}
+                    />
 
-        <XIcon
-            className='h-7 text-gray-500 cursor-pointer 
-            sm:mr-3 transition duration-100 transform hover:scale-125'
-            onClick={()=> searchInputRef.current.value=''}
-        />
-        <MicrophoneIcon
-            className={'h-6 mr-3 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300'}
-        />
-        <SearchIcon
-            className='h-6 text-blue-500 hidden sm:inline-flex'
-        />
+                    <XIcon
+                        className='h-7 text-gray-500 cursor-pointer 
+                        sm:mr-3 transition duration-100 transform hover:scale-125'
+                        onClick={()=> searchInputRef.current.value=''}
+                    />
+                    <MicrophoneIcon
+                        className={'h-6 mr-3 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300'}
+                    />
+                    <SearchIcon
+                        className='h-6 text-blue-500 hidden sm:inline-flex'
+                    />
 
-        <button hidden type='submit' onClick={search} className=''>search</button>
-        </form>
+                    <button hidden type='submit' onClick={search} className=''>search</button>
+                    </form>
 
-        <Button
-            className='ml-auto'
-        />
+                    <Button
+                        className='ml-auto'
+                    />
+
+            </div>
 
         </div>
 
