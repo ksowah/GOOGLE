800fb0c9db4e41b48917dd68fcd005c171d79178
ksowah <sowahkelvin640@gmail.com>
2022-02-11 18:02:28 +0000

    update

--- components/Button.js
+++ components/Button.js
@@ -19,13 +19,19 @@ const Button = ({className}) => {
   }, [])
 
   return (
+            <>
+                {
+                  loginState ? 'hello' :
 
-        <button 
-          onClick={()=>signInWithPopup(auth, provider) .catch(error => alert(error.code))}
-            className={`px-5 py-2 bg-sky-600 rounded-lg cursor-pointer text-white font-bold 
-          hover:bg-sky-500 ${className}`}>
-            Sign in
-        </button>
+                  <button 
+                  onClick={()=>signInWithPopup(auth, provider) .catch(error => alert(error.code))}
+                  className={`px-5 py-2 bg-sky-600 rounded-lg cursor-pointer text-white font-bold 
+                  hover:bg-sky-500 ${className}`}>
+                  Sign in
+                  </button>
+                }
+            </>
+        
    
   )
 }
