d08bdcd08c2f51d047b424c18387743a6060bb1f
ksowah <sowahkelvin640@gmail.com>
2022-02-11 17:49:01 +0000

    update

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADuD3OAX9EOBPtd2hagdqo8ETnxsS8oZY",
+  authDomain: "gg-clone-1644506184665.firebaseapp.com",
+  projectId: "gg-clone-1644506184665",
+  storageBucket: "gg-clone-1644506184665.appspot.com",
+  messagingSenderId: "250768798887",
+  appId: "1:250768798887:web:14c91029241628da19d782",
+  measurementId: "G-8Z0QWVT163"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
