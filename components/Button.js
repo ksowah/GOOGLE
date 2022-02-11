648f0dda8f34db8930a76b1a7a77174781c40501
ksowah <sowahkelvin640@gmail.com>
2022-02-11 17:49:49 +0000

    update

--- components/Button.js
+++ components/Button.js
@@ -2,7 +2,11 @@
 const Button = ({className}) => {
   return (
     
-        <button className={`px-5 py-2 bg-sky-600 rounded-lg cursor-pointer text-white font-bold hover:bg-sky-500 ${className}`}>Sign in</button>
+        <button 
+            className={`px-5 py-2 bg-sky-600 rounded-lg cursor-pointer text-white font-bold 
+          hover:bg-sky-500 ${className}`}>
+            Sign in
+        </button>
    
   )
 }
